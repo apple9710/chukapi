9cc1949bc19bd9b28d9a575ca780c3b0e50d0946
ParkJunYoung <rkt1313@naver.com>
2025-01-10 17:41:22 +0900

    빌드

---
+++ src/pages/callendar.jsx
@@ -0,0 +1,22 @@
+import data from '../../data.json'
+const Callendar = ()=> {
+
+  console.log(data)
+
+
+
+
+  return (
+    <>
+      <h1>에어드랍 캘린더</h1>
+    
+
+
+    </>
+
+  )
+
+}
+
+
+export default Callendar;
